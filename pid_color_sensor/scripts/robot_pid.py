dee86bf56c785ff5f954ed597f6f80f314b39268
aymeP <58934999+aymeP@users.noreply.github.com>
2022-06-16 16:33:03 +0200

    Update parameters

--- pid_color_sensor/scripts/robot_pid.py
+++ pid_color_sensor/scripts/robot_pid.py
@@ -2,7 +2,7 @@
 
 import rospy
 from geometry_msgs.msg import Twist
-from std_msgs.msg import Float32
+from std_msgs.msg import Float32, String
 
 angle_order = 0
 robot_linear_speed = 0
@@ -10,25 +10,50 @@ robot_angular_speed = 0
 propotionnal_constant = 0
 integration_constant = 0
 derivation_constant  = 0
-sub_topic_name = ""
+error_topic_name = ""
+color_topic_name = ""
 
 robot_command = Twist()
 
 command_publisher = None
 subscriberError = None
 
+cumulated_error = 0
+previous_error = 0
 
 def P(measure_angle):
+    global angle_order
+    rospy.loginfo(measure_angle)
     error = measure_angle - angle_order
     return propotionnal_constant * error
 
+def PI(measure_angle):
+    global angle_order
+    error = measure_angle - angle_order
+    global cumulated_error
+    cumulated_error += error
+    return P(measure_angle) + integration_constant * cumulated_error
+
+def PID(measure_angle):
+    global angle_order
+    error = measure_angle - angle_order
+    global previous_error
+    correction_order = PI(measure_angle) + derivation_constant*(error - previous_error)
+    previous_error = error
+    return correction_order
+
 def callbackError(msg):
     robot_command.angular.z = P(msg.data);
+    #robot_command.angular.z = PI(msg.data)
+    #robot_command.angular.z = PID(msg.data)
     command_publisher.publish(robot_command);
 
+def callbackColor(msg):
+    pass
 
-def RobotPID() : #cumulated_error(0), previous_error(0)
-    global angle_order, robot_linear_speed, robot_angular_speed, propotionnal_constant, integration_constant, derivation_constant, sub_topic_name, robot_command, subscriberError, command_publisher
+
+def RobotPID() :
+    global angle_order, robot_linear_speed, robot_angular_speed, propotionnal_constant, integration_constant, derivation_constant, error_topic_name, color_topic_name, robot_command, subscriberError, command_publisher
 
     angle_order = rospy.get_param("/robotPID/angle_order")
 
@@ -37,17 +62,13 @@ def RobotPID() : #cumulated_error(0), previous_error(0)
     propotionnal_constant = rospy.get_param("/robotPID/propotionnal_constant")
     integration_constant = rospy.get_param("/robotPID/integration_constant")
     derivation_constant = rospy.get_param("/robotPID/derivation_constant")
-    sub_topic_name = rospy.get_param("/robotPID/sensor_topic")
-
+    error_topic_name = rospy.get_param("/robotPID/error_topic")
+    color_topic_name = rospy.get_param("/robotPID/color_topic")
 
     rospy.init_node('color_sensor_pid_node')
 
-
-
-    #Verifier que le nom du topic est le bon
-    subscriberError = rospy.Subscriber(sub_topic_name, Float32, callbackError)
-    #Faire pareil avec Color
-
+    subscriberError = rospy.Subscriber(error_topic_name, Float32, callbackError)
+    subscriberColor = rospy.Subscriber(color_topic_name, String, callbackColor)
 
     command_publisher = rospy.Publisher('cmd_vel', Twist, queue_size=1000) 
 
@@ -63,6 +84,8 @@ def RobotPID() : #cumulated_error(0), previous_error(0)
 
 RobotPID()
 rospy.spin()
+
+
 """
 double RobotPID::bang_bang(double measure_angle){
     double error = measure_angle - angle_order;
@@ -71,38 +94,5 @@ double RobotPID::bang_bang(double measure_angle){
         return -robot_angular_speed;
  
     return robot_angular_speed;
-}
-"""
-
-
-
-#A transformer
-"""
-double RobotPID::P(double measure_angle){
-    double error = measure_angle - angle_order;
-    double calculated_order;
-    
-    return propotionnal_constant * error;
-
-}
-"""
-
-"""
-double RobotPID::PI(double measure_angle){
-    double error = measure_angle - angle_order;
-
-    cumulated_error += error;
-
-    return P(measure_angle) + integration_constant * cumulated_error;
-}
-
-double RobotPID::PID(double measure_angle){
-    double error = measure_angle - angle_order;
-
-    double correction_order = PI(measure_angle) + derivation_constant*(error - previous_error);
-
-    previous_error = error;
-
-    return correction_order;
 }
 """
